ca5bbb4be4bb85ad76ec6465f799128e88d6de80
iye <iye@riotgames.com>
2020-07-14 18:55:51 +0900

    Controller의 GET method들에 @CrossOrigin annotation을 추가합니다

--- src/main/java/org/ajou/realcoding/weathercrawler/controller/CurrentWeatherController.java
+++ src/main/java/org/ajou/realcoding/weathercrawler/controller/CurrentWeatherController.java
@@ -3,6 +3,7 @@ package org.ajou.realcoding.weathercrawler.controller;
 import org.ajou.realcoding.weathercrawler.domain.CurrentWeather;
 import org.ajou.realcoding.weathercrawler.service.CurrentWeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class CurrentWeatherController {
     @Autowired
     private CurrentWeatherService currentWeatherService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/weather-service/weathers")
     public CurrentWeather getCurrentWeather(@RequestParam String cityName) {
         return currentWeatherService.getCurrentWeather(cityName);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/weather-service/available-cities")
     public List<String> getAvailableCities() throws IOException {
         return currentWeatherService.getAvailableCities();
